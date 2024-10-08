66206070279a1f3495943c101bb1c355797b2014
beraaritra <aritra.bera@lawsikho.in>
2024-10-08 16:13:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import colors from "colors";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 7000;
+
+app.get('/', (req, res) => {
+    return res.json({ message: 'Hello from Express.js!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`.bgBlue);
+});
